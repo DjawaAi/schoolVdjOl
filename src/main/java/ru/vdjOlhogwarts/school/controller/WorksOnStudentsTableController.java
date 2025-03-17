94cbca9a16a0cb229ad5f95fba02bada9a8ec243
Dm&try <dmitrypanyukov@gmail.com>
2025-03-18 00:54:52 +0300

    3.6 Д.З. 4.1 Выполнил

---
+++ src/main/java/ru/vdjOlhogwarts/school/controller/WorksOnStudentsTableController.java
@@ -0,0 +1,34 @@
+package ru.vdjOlhogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.vdjOlhogwarts.school.model.Student;
+import ru.vdjOlhogwarts.school.model.WorksOnStudentsTable;
+import ru.vdjOlhogwarts.school.service.StudentService;
+
+import java.util.List;
+
+@RestController
+public class WorksOnStudentsTableController {
+
+    private final StudentService studentService;
+
+    public WorksOnStudentsTableController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/student-number")
+    public Long getNumberOfStudents() {
+        return studentService.getNumberOfStudents();
+    }
+
+    @GetMapping("/student-avg-age")
+    public WorksOnStudentsTable getAverageAgeOfStudents() {
+        return studentService.getAverageAgeOfStudents();
+    }
+
+    @GetMapping("/student-last-five")
+    public List<Student> getLastFiveStudentsInTable() {
+        return studentService.getLastFiveStudentsInTable();
+    }
+}
--- src/main/java/ru/vdjOlhogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/vdjOlhogwarts/school/repository/StudentRepository.java
@@ -1,12 +1,25 @@
 package ru.vdjOlhogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.vdjOlhogwarts.school.model.Student;
+import ru.vdjOlhogwarts.school.model.WorksOnStudentsTable;
 
 import java.util.List;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByAge(int age);
 
     List<Student> findByAgeBetween(int minAge, int maxAge);
+
+    @Query(value = "SELECT COUNT(id) FROM student", nativeQuery = true)
+    Long getStudentsNumber();
+
+    @Query(value = "SELECT AVG(age) AS ageAVG FROM student", nativeQuery = true)
+    WorksOnStudentsTable getAverageAgeOfStudents();
+
+    @Query(value = "SELECT * FROM (SELECT * FROM student ORDER BY id DESC LIMIT 5) AS lastStudents ORDER BY id ASC", nativeQuery = true)
+    List<Student> getLastFiveStudentsInTable();
 }
